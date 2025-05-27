b48460a8dd9aa9898613efd167249f814a2c2b4d
Joao Bornelli <joao.bornelli@edu.sc.senai.br>
2025-05-26 21:57:49 -0300

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
- 
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+
+audiofile = 'AudioFile.m4a'
+
+y,sr = librosa.load(audiofile,
+                    sr=None,
+                    offset=1.0,
+                    duration = 0.2)
+S = np.abs(librosa.stft(y,
+                        n_fft=1024,
+                        hop_length = 256))
+
+
+print (S.shape)
+
+fig,ax = plt.subplots(dpi=300)
+img = librosa.display.specshow(
+    librosa.amplitude_to_db(
+        S,
+        ref = np.max,),
+    y_axis='log',
+    x_axis='time',
+    ax=ax,)
+plt.savefig("spectrum.png",dpi=300)
