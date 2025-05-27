319f491e664d9a83afcc9c2514b09a4ad50edd7c
Joao Bornelli <joao.bornelli@edu.sc.senai.br>
2025-05-27 19:08:14 -0300

    Implement code changes to enhance functionality and improve performance

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 audiofile = 'AudioFile.m4a'
 
-y,sr = librosa.load(audiofile,
-                    sr=None,
-                    offset=1.0,
-                    duration = 0.2)
-S = np.abs(librosa.stft(y,
-                        n_fft=1024,
-                        hop_length = 256))
+
+y, sr = librosa.load(audiofile, sr=None)
+print(f"Sample rate: {sr}")
+
+n_fft = 2048
+hop_length = 256
 
 
-print (S.shape)
+S = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length))
+print("Shape da STFT (freqs x tempo):", S.shape)
 
-fig,ax = plt.subplots(dpi=300)
+df = pd.DataFrame(S)
+print(S)
+
+plt.figure(figsize=(12, 4), dpi=300)
+
 img = librosa.display.specshow(
-    librosa.amplitude_to_db(
-        S,
-        ref = np.max,),
-    y_axis='log',
-    x_axis='time',
-    ax=ax,)
-plt.savefig("spectrum.png",dpi=300)
+    librosa.amplitude_to_db(S, ref=np.max),
+    sr=sr,
+    hop_length=hop_length,
+    y_axis='linear',
+    x_axis='time'
+)
+
+plt.savefig("spectrum.png", dpi=300)
+df.to_csv('Spectrum.csv')
+
